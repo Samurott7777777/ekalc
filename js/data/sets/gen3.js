5f1ecdbd221f3e9075e7a3d6bc6d053e0837cd23
Samurott7777777 <58022381+Samurott7777777@users.noreply.github.com>
2024-11-25 18:20:43 -0800

    ILikeSamurott

--- js/data/sets/gen3.js
+++ js/data/sets/gen3.js
@@ -1580,7 +1580,7 @@ var SETDEX_ADV = {
             ]
         },
         "Grunt 7": {
-            "level": 45,
+            "level": 43,
             "ivs": {
                 "hp": 31,
                 "at": 31,
